467db47978f19ea6bb2321a0c6f9ae70c0e1b1dd
Nishikantaray <nishikantaray1@gmail.com>
2022-03-23 20:20:16 +0530

    added

--- js/index.js
+++ js/index.js
@@ -208,3 +208,16 @@ function fileValidate(fileType, fileSize) {
 };
 
 // :)
+
+document.querySelectorAll('.dropdown-toggle').forEach(item => {
+    item.addEventListener('click', event => {
+   
+      if(event.target.classList.contains('dropdown-toggle') ){
+        event.target.classList.toggle('toggle-change');
+      }
+      else if(event.target.parentElement.classList.contains('dropdown-toggle')){
+        event.target.parentElement.classList.toggle('toggle-change');
+      }
+    })
+  });
+  
